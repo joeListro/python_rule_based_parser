f2d786a76c98ff85257cac9c7c30691b498b0da6
joeListro <jlistro@gmail.com>
2017-04-11 10:19:52 -0400

    Add files

---
+++ python_rule_based_parser/model/pythonCodeModel.py
@@ -0,0 +1,5 @@
+class pythonCodeModel:
+    """ Hold the code to be parsed in a model """
+
+    def __init__(self):
+        return super().__init__()
---
+++ python_rule_based_parser/utilities/fileLoader.py
@@ -0,0 +1,8 @@
+class fileLoader:
+    """Load text from file"""
+
+    def __init__(self):
+        return super().__init__()
+
+    def load_file(self, pathToFile):
+        """ TODO : Load file """
