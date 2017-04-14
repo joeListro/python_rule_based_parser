607c680d05895f7339012c3bd5c0b7a9dfaf7fa6
joeListro <jlistro@gmail.com>
2017-04-14 10:26:01 -0400

    added tokenizer.py

---
+++ python_rule_based_parser/utilities/tokenizer.py
@@ -0,0 +1,63 @@
+class tokenizer:
+    """"Tokenizes a code string"""
+
+    def __init__(self):
+        return super().__init__()
+
+    def tokenizer(self):
+
+        return
+
+    def isSpaceOrTab(self, currentChar):
+
+        if (currentChar == ' '):
+            return True
+        if (currentChar == '\t'):
+            return True
+
+        return False
+
+    def isNewLine(self, currentChar):
+
+        if (currentChar == '\n'):
+            return True
+        
+        return False
+
+    def isCarriageReturn(self, currentChar):
+
+        if (currentChar == '\r'):
+            return True
+
+        return False
+
+    def isSingleToken(self, currentChar):
+
+        if (currentChar == '{'):
+            return True
+        if (currentChar == '}'):
+            return True
+        if (currentChar == '['):
+            return True
+        if (currentChar == ']'):
+            return True
+        if (currentChar == '('):
+            return True
+        if (currentChar == ')'):
+            return True
+
+        return False
+
+    def isDoubleToken(self, currentChar):
+
+        if (currentChar == '{'):
+            return False
+
+        return True
+
+    def isIdentifierChar(self, currentChar):
+
+        if(self.isSingleToken(currentChar) or self.isSpaceOrTab(currentChar) or self.isNewLine(currentChar) or self.isCarriageReturn(currentChar)):
+            return False
+
+        return True
